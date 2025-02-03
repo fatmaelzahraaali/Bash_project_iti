c4b84804172ae4271c00eb8feaa0d66929e9d910
fatmaelzahraaali <fatma.elzahraa.ali.75@gmail.com>
2025-02-03 16:07:50 +0200

    create with gui

--- functions/functions.sh
+++ functions/functions.sh
@@ -1,24 +1,91 @@
 #!/bin/bash 
+RED='\033[1;31m'	          # Red Color code 
+Green='\033[1;32m'	        # Green Color Green
+ColorReset='\033[0m' 		    # No Color Code
 
 mainMenu()
 {
-	select opt in "Create Database" "Connect to Database" "List Database" "Drop Database" "Exit"
-	do
-		case $opt in 
-			"Create Database") . ./main_menu/create_db.sh
-				;;
-			"Connect to Database") . ./main_menu/connect_db.sh
-				;;
-			"List Database") . ./main_menu/list_db.sh
-                                ;;
-			"Drop Database") . ./main_menu/drop_db.sh
-		        	;;
-		        "Exit") exit
-				;;
-			*) " invalid option ! "
-		esac
-	done
+     choice=$(zenity --list \
+         --height="450"\
+         --width="400"\
+         --cancel-label="Exit" \
+         --title="Main Menu" \
+         --column="Option" \
+         "Create Database" \
+         "List Database" \
+         "Connect To Database" \
+         "Drop Database")
+
+     if [ $? -eq 1 ]         #Checks if the exit status of the last command is 1
+     then
+          echo -e "${Green}Exited..${ColorReset}"
+          exit
+     fi
+ 
+     case $choice in 
+		"Create Database") ./main_menu/create_db.sh;;
+		"List Database") ./main_menu/list_db.sh;;
+		"Connect To Database") ./main_menu/connect_db.sh;;
+		"Drop Database") ./main_menu/drop_db.sh;;
+		*) echo -e "${RED}invalid choice, try again ... you must choose only from the above list${ColorReset}";
+
+        	mainMenu          #Call it again
+       esac
 }
+
+
+############################### creating db functions #########################
+### this function checks if the database already exist or not ####
+
+isDatabaseExist()
+{
+  if [ -d ./Databases/$1 ]
+  then
+	  return 0          #returns  0 if true (exist)
+  else
+	  return 1          #returns 1 if false (not exist)
+  fi
+
+}
+####### this function creates database #######
+
+createDatabase()
+{
+
+  mkdir ./Databases/$1
+  mkdir ./Databases/$1/.metadata
+
+}
+
+listDatabases() 
+{
+    
+
+    while true;
+    do
+        # List available databases and prompt user to select one
+        dbName=$(ls -l Databases | grep "^d" | awk '{print $9}' | zenity --cancel-label="Back" --list --height="450" --width="400" --title="Database List" --text="Select your database" --column="Database name")
+        
+        
+        if [ $? -eq 1 ] || [$? -eq 0];
+       	then
+            mainMenu
+            return
+        fi
+        if [ -z "$dbName" ]
+       	then
+            zenity --error --width="230" --text="Database field cannot be empty"
+        else
+            if isDatabaseExist "$dbName";
+	    then
+                break
+            else
+                zenity --error --width="200" --text="Database [$dbName] does not exist"
+            fi
+        fi
+    done
+}
+
 
 db_menu()
 {
--- main_menu/create_db.sh
+++ main_menu/create_db.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
 
-# Take the db name from the user
-read -p "Enter a name for the DataBase: " db_name
+. ./functions/functions.sh
 
-# The main directory of databases
-db_dir="./databases"
+while true
+do
+  
+  dbName=$(zenity --entry \
+  --title="Add new database" \
+  --text="Enter database name:" \
+  --cancel-label="cancel"\
+  --entry-text "DB_name")
 
-# Check if it exists or not
-if [ -d "$db_dir/$db_name" ]
-then
-	echo "This DataBase is already exists."
-elif [ ! -d "$db_dir/$db_name" ]
-then
-	mkdir "$db_dir/$db_name"
-	echo "DataBase '$db_name' is created successfully."
-else 
-	echo "Invalid input." 
-	exit
-fi	
+# if cancel return to mainMenu 
+  if [[ $? = 1 ]]
+  then
+      mainMenu
+  fi 
+
+
+  if [[ -z "$dbName" ]] || [[ ! $dbName =~  ^[a-zA-Z]+[a-zA-Z0-9]*$ ]] 
+  then
+      zenity --error --width="300" --text="Database field cannot be empty or start with space or number or special char"
+  else
+      # check if the database is exit or not
+      if isDatabaseExist $dbName
+      then
+          zenity --error --width="200" --text="[$dbName] is already exist"   
+      else
+          createDatabase $dbName
+
+          # check if last command is Done(a datebase name is entered successfully
+          if [ $? -eq 0 ]
+          then
+              
+              zenity --info --width="200" --text="[$dbName] created successfully"
+              break 
+	  else   
+              zenity --error --width="200" --text="Error occured during creating the database"   
+
+          fi
+
+        fi
+      fi 
+done
+
+mainMenu
