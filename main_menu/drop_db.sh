07533e841d12fe5f96c7ee96e032186bc895d54e
fatmaelzahraaali <fatma.elzahraa.ali.75@gmail.com>
2025-02-07 20:49:52 +0200

    drop

--- main_menu/drop_db.sh
+++ main_menu/drop_db.sh
@@ -2,12 +2,6 @@
 . ./functions/functions.sh
 
 
-if [ $? -eq 1 ]
-then
-   mainMenu
-fi
-
-
 # check if there is any database found to be deleted 
 if [ -z "$(ls -A Databases)" ];
   then
@@ -20,8 +14,14 @@ fi
 # listing the database       
 while true;
 do
+	
       dbName="$(ls -l Databases | grep "^d" | awk -F ' ' '{print $9}' | zenity --list --height="400" --width="400" --cancel-label="Back"  --title="Database List" --text="Select your database"  --column="Database name" 2>>.errorlog)"
-if [[ -z $dbName ]];
+if [ $? -eq 1 ]
+then
+   mainMenu
+fi
+
+      if [[ -z $dbName ]];
 then
      zenity --error --width="200" --text="Database Doesnot exist"
 else
