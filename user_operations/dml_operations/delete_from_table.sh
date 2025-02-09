68e6945321ce2cd3fbd4857df07383c416e5af50
MaramEl-Sayed <maram20865@fci.bu.edu.eg>
2025-02-09 15:35:40 +0200

    delete check on primary key

--- user_operations/dml_operations/delete_from_table.sh
+++ user_operations/dml_operations/delete_from_table.sh
@@ -4,10 +4,11 @@
 deleteFromTable() {
     while true; do
         # Ask for the table name
-	tableName=$(ls -l ./Databases/$1 | grep "^d" | awk '{print $9}' | zenity --list --height="400" --width="400" --title="Select Table" --column="Table Name")
+        tableName=$(ls -l ./Databases/$1 | grep "^d" | awk '{print $9}' | zenity --list --height="400" --width="400" --title="Select Table" --column="Table Name")
+        
         if [[ $? -eq 1 ]]; then
-           db_menu $1
-	       	return  # Exit if user presses cancel
+            db_menu $1
+            return  # Exit if user presses cancel
         fi
 
         if [[ -z "$tableName" ]]; then
@@ -24,12 +25,17 @@ deleteFromTable() {
                 if [[ -z "$primaryKey" ]]; then
                     zenity --error --width="300" --text="Primary key cannot be empty."
                 else
-                    # Delete row where the first column (primary key) matches the entered value
-                    awk -F, -v key="$primaryKey" 'NR==1 || $1 != key' "$tableFile" > temp.txt && mv temp.txt "$tableFile"
+                    # Check if the primary key exists in the table
+                    if ! grep -q "^$primaryKey," "$tableFile"; then
+                        zenity --error --width="300" --text="Row with Primary Key [$primaryKey] does not exist."
+                    else
+                        # Delete row where the first column (primary key) matches the entered value
+                        awk -F, -v key="$primaryKey" 'NR==1 || $1 != key' "$tableFile" > temp.txt && mv temp.txt "$tableFile"
 
-                    zenity --info --width="200" --text="Row with Primary Key [$primaryKey] deleted from [$tableName]."
-		   db_menu $1
-		    break  # Exit loop after successful deletion
+                        zenity --info --width="200" --text="Row with Primary Key [$primaryKey] deleted from [$tableName]."
+                        db_menu $1
+                        break  # Exit loop after successful deletion
+                    fi
                 fi
             fi
         fi
@@ -37,4 +43,3 @@ deleteFromTable() {
 }
 
 deleteFromTable $1
-
